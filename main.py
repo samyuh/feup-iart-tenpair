52749552bbc4a4ec3315931f6026e98959fbd000
up201806490 <up201806490@fe.up.pt>
2021-03-12 19:07:54 +0000

    depthDeepening not working yet

--- main.py
+++ main.py
@@ -200,6 +200,73 @@ def depthFirstSearch(game):
     end = time.time()
     print("Time elapsed: {}".format(end - start))
 
+
+def iterativeDeepeningAux(game, depth):
+    """
+        Iterative Deepening Algorithm
+    """
+    ai = Ai()
+    # rows, columns, gameState = deal(rows, columns, gameState.copy())
+    # Double Ended Queue to allow O(1) pop and append
+    # Set to check if an element was already visited in O(1)
+    queue = deque([game])
+    visited = set()
+
+    start = time.time()
+    while True:
+
+        if (len(visited) % 10000 == 0):
+            print("Visited: {} Remaining: {}".format(len(visited), len(queue)))
+        
+        if  len(queue) == 0:
+            print("empty queue")
+            return False
+        # Next GameState
+        game = queue.pop()
+        #game.printGame()
+
+        
+        if game.moves + 1 == depth:
+            print("Maximum depth reached for this node")
+            continue  
+
+        # Found a solution [Empty Matrix]
+        if game.isEmpty():
+            print("Found a solution: ")
+            print("Total Moves: {}".format(game.moves))
+            return True
+        # GameState Already Visited
+        elif repr(game.matrix) in visited:
+            pass
+        # Get available moves and add them to the queue
+        else:
+            visited.add(repr(game.matrix))
+            append = queue.append
+
+            operationList = ai.getAllMoves(game.rows, game.columns, game.matrix)
+            #removePair = game.removePair
+
+            newGameMoves = game.moves + 1
+            for operation in operationList:
+                newGame = Game(newGameMoves,game.dealValue, game.rows, game.columns, game.matrix.copy())
+                newGame.removePair(operation[0], operation[1])
+                append(newGame)
+
+            if game.dealValue < 1:
+                rowsDeal, columnsDeal, gameStateDeal = game.deal(game.rows, game.columns, game.matrix.copy())
+                gameDeal = Game(game.moves, game.dealValue + 1, rowsDeal, columnsDeal, gameStateDeal)
+                append(gameDeal)
+
+    end = time.time()
+    print("Time elapsed: {}".format(end - start))
+
+    
+def iterativeDeepening(game, maxDepth):
+    for i in range(25, maxDepth):
+        print("depth", i ,"attempt") 
+        if (iterativeDeepeningAux(game, i)):
+            return True
+
 if __name__ == "__main__":
     gameState = [1, 2, 3, 4, 5, 6, 7, 8, 9,
                 1, 1, 1, 2, 1, 3, 1, 4, 1, 
@@ -209,6 +276,6 @@ if __name__ == "__main__":
     game = Game(0, 0, rows, columns, gameState)
     game.heuristic = greedyHeuristic(gameState)
 
-    aStar(game)
+    iterativeDeepening(game,30)
 
     
