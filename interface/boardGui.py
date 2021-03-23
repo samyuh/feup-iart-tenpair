f563cad180e4c30a80cc9e552e8aa16367c99c4a
Diana Freitas <up201806230@fe.up.pt>
2021-03-23 23:58:34 +0000

    Extend Gui board (for deals)
    TODO: integrate and define frame size as desired

--- interface/boardGui.py
+++ interface/boardGui.py
@@ -3,17 +3,6 @@ import tkinter as tk
 from tkinter import Button
 from tkinter.font import Font
 
-class Game:
-    """
-    Game manager. Includes user actions
-    """
-    def __init__(self, moves, dealValue, rows, columns, matrix): 
-        self.moves = moves
-        self.dealValue = dealValue
-        self.columns = columns
-        self.rows = rows
-        self.matrix = matrix
-
 class Gui(tk.Frame):
     def __init__(self, states):
         tk.Frame.__init__(self)
@@ -24,8 +13,12 @@ class Gui(tk.Frame):
         self.move_count = 0
 
         first_state = self.states[self.actual_state]
+
+        # todo: adjust frame size as desired
         self.main_grid = tk.Frame(self, bg = "#c7c7c7", bd = "3", width = str(100 * first_state.columns), height = str(100 * first_state.rows))
         self.main_grid.grid(pady=(100,100), padx=(100,100))
+
+        # Create Gui and represent first state
         self.make_GUI(first_state)
         self.update_GUI()
 
@@ -68,6 +61,10 @@ class Gui(tk.Frame):
     def update_GUI(self):
         state = self.states[self.actual_state] # actual state
 
+        # Extend board
+        if len(state.matrix) > len(self.cells) * len(self.cells[0]):
+            self.extend_board(state)
+
         for index, cell_value in enumerate(state.matrix):
             col = index % state.columns
             row = index // state.columns
@@ -78,16 +75,6 @@ class Gui(tk.Frame):
             else:                
                 self.cells[row][col]["frame"].configure(bg="#ffffff")
                 self.cells[row][col]["number"].configure(text = str(cell_value))
-
-        for index in range(state.columns * state.rows - len(state.matrix)):
-            col = index % state.columns
-            row = index // state.rows
-
-            self.cells[row][col]["frame"].configure(bg="#ffffff")
-            self.cells[row][col]["number"].configure(text="")
-
-        # Update cells if length > 
-        self.main_grid.configure(width = str(100 * state.columns), height = str(100 * state.rows))
 
         self.actual_state += 1
         self.moves = state.moves
@@ -103,9 +90,32 @@ class Gui(tk.Frame):
             # Change this to go to main menu
             self.moveBtn['command'] = self.main_grid.destroy
 
+    def extend_board(self,state):
+        prev_rows = len(self.cells)
+        prev_cols = len(self.cells[0])
 
+        # Number of rows to add
+        extend_rows = (len(state.matrix) - prev_rows * prev_cols) // state.columns + 1
 
-gameState1 = [1, 2, 3, 4, 5, 6, 7, 8, 9,
+        for i in range(prev_rows, prev_rows + extend_rows):
+            row = []
+            for j in range(state.columns):
+                cell_frame = tk.Frame(
+                    self.main_grid,
+                    bg = "#f2f2f2",
+                    width = 100,
+                    height = 100
+                )
+                cell_frame.grid(row=i, column=j, padx=5, pady=5)
+                cell_number = tk.Label(self.main_grid, bg="#F0F0F0", font="Helvetica")
+                cell_number.grid(row=i,column=j)
+                cell_data = {"frame": cell_frame, "number": cell_number}
+                row.append(cell_data)
+            self.cells.append(row)
+
+        
+# Testing with 2 boards with different rows
+""" gameState1 = [1, 2, 3, 4, 5, 6, 7, 8, 9,
                 1, 1, 1, 2, 1, 3, 1, 4, 1, 
                 5, 1, 6, 1, 7, 1, 8, 1, 9]
 columns = 9
@@ -113,11 +123,11 @@ rows = 3
 
 state1 = Game(0, 0, rows, columns, gameState1)
 
-gameState2 = [1, 2, 
-3, 4, 5, 6, 7, 8, None,
+gameState2 = [1, 2, 3, 4, 5, 6, 7, 8, None,
                 None, 1, 1, 2, 1, 3, 1, 4, 1, 
-                5, 1, 6, 1, 7, 1, 8, 1, 9]
+                5, 1, 6, 1, 7, 1, 8, 1, 9, 
+                7, 8, 9]
 
-state2 = Game(1, 0, rows, columns, gameState2)
+state2 = Game(1, 0, 4, columns, gameState2)
 
-Gui([state1,state2])
+Gui([state1,state2]) """
